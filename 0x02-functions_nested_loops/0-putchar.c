d3f23b13a9b437508f4b0933bf6495e72ce3b562
Emmanuel <ubryliant@gmail.com>
2022-09-13 19:23:44 -0700

    added file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - program that prints _putchar, followed by a new line.
+ *
+ * Return: always success/true
+ */
+
+int main(void)
+{
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+File 0-putchar.c: program that prints _putchar, followed by a new line.
+
+

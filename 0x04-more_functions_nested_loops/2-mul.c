5d46afab549835d341559a748dc480ec413352b6
Emmanuel <ubryliant@gmail.com>
2022-09-16 14:16:15 -0700

    added file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers.
+ *
+ * @a: the arguement for multiplication
+ * @b: the arguement for multiplication
+ *
+ * Return: success
+ */
+
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+return (c);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,4 +2,6 @@ File 0-isupper.c: function that checks for uppercase character.
 
 File 1-isdigit.c: function that checks for a digit (0 through 9).
 
+File 2-mul.c: function that multiplies two integers.
+
 

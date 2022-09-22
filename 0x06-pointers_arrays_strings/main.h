fef111c0915e557d30c2ae8cafbe2386c3ed3aff
Emmanuel <ubryliant@gmail.com>
2022-09-22 06:50:12 -0700

    updated file main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
@@ -7,3 +10,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 
+#endif

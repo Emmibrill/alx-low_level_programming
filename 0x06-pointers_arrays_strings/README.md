d316e3265a6414f2b2999c561005c7447fa1b4f8
Emmanuel <ubryliant@gmail.com>
2022-09-22 07:04:33 -0700

    added file 1-strncat.c

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,3 @@
 File 0-strcat.c: function that concatenates two strings.
 
-
+File 1-strncat.c: function that concatenates two strings.
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
@@ -9,5 +6,3 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-
-#endif
